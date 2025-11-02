d0ac22053d3b5a971b5b50e49c6db09dcc06ee1e
kamilibragimov <kamil.ibragimov@studenti.isissgobetti.it>
2025-11-02 15:30:52 +0100

    Aggiunta classe Moto che eredita da Veicolo

---
+++ progetto-veicoli/moto.py
@@ -0,0 +1,6 @@
+from veicolo import Veicolo
+
+class Moto(Veicolo):
+    def __init__(self, marca, modello, cilindrata):
+        super().__init__(marca, modello)
+        self.cilindrata = cilindrata
